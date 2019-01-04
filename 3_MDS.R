b4a2b4cb65106f5c0df25228878db6eee3b58426
Alexandra Fraik <alexandra.fraik@wsu.edu>
2019-01-03 16:39:37 -0800

    Update 3_MDS.R

--- 3_MDS.R
+++ 3_MDS.R
@@ -35,3 +35,25 @@ DGE_Object$samples
 
 # Create a dataframe with the normalized library counts 
 data.frame<-cpm(DGE_Object,normalized.lib.sizes=TRUE,log=TRUE,prior.count=1)
+
+## Plot the MDS of the top 1,000 genes or the top genes differentially expressed across all samples
+# color by your groupings factor and create labels with you sample information
+# Sample_Names <- Names of your samples (i.e: 1:20)
+# colors <- colors for grouping factors
+# pch <- "pch" or vector of values for the shape you want for a grouping factor
+pdf(file="Top_1000_Sample_Names.pdf")
+plotMDS(DGE_Object,top=1000,col=colors[groupings],labels=Sample_Names,gene.selection="common")
+legend(3,1,legend=levels(groupings),cex=1,pch=points,col=colors,pt.bg=colors,ncol=1)
+legend.text=TRUE
+dev.off()
+
+## Plot the MDS without the sample names, just have points with colors and shapes representing grouping factors
+# use the top 500, 1000, 2000, 10000 genes and save a different jpeg
+values<-c(500,1000,2000,10000)
+for ( i in values ) {
+jpeg(paste(i,'MDS_Plots_sn.jpg',sep= '_'))
+plotMDS(DGE_Object,top=1000,col=colors[groupings],pch=points[groupings],bg=colors[groupings],gene.selection="common")
+legend(3,1,legend=levels(groupings),cex=1,pch=points,col=colors,pt.bg=colors,ncol=1)
+legend.text=TRUE
+dev.off()
+}
