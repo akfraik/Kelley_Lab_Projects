7a5c3c6449be7c3ef901680804ea149cfebf835f
Alexandra Fraik <alexandra.fraik@wsu.edu>
2019-01-10 16:27:27 -0800

    Update 1_SNP_Calling.sh

--- Devil_Transcriptome_Uninfected.md/Additional_Scripts/1_SNP_Calling.sh
+++ Devil_Transcriptome_Uninfected.md/Additional_Scripts/1_SNP_Calling.sh
@@ -55,6 +55,9 @@ java -jar GenomeAnalysisTK.jar -T SplitNCigarReads -R $REFDIR/ref.fa -I $MAPDIR/
 # Echo the sample that completes 
 echo ${i}_split.bam
 
+# Remove temporary files and zip up the others to reduce space
+rm $MAPDIR/${i}_dedupped.bam
+
 done
 
 ###########################################################################################################################
@@ -69,3 +72,69 @@ for i in 1..20;
 
 do
 
+# Command for BQRS
+java -jar GenomeAnalysisTK.jar -T BaseRecalibrator -R $REFDIR/ref.fa -I $MAPDIR/${i}_split.bam 
+-o $MAPDIR/${i}_recal_data.table --run_without_dbsnp_potentially_ruining_quality
+
+# Recreate recalibrate BAM files with improved scores
+java -jar GenomeAnalysisTK.jar -T PrintReads -R $REFDIR/ref.fa -I $INDIR/${i}_split.bam 
+-BQSR $MAPDIR/${i}_recal_data.table -o $MAPDIR/${i}_recal.bam
+
+#Echo the sample that completes 
+echo $MAPDIR/${i}_recal.bam
+
+samtools index $MAPDIR/${i}_recal.bam
+
+done
+
+###########################################################################################################################
+# 1d. Variant Calling
+###########################################################################################################################
+
+# $REFDIR = reference annotation directory
+module load java
+
+#Create a directory or designate a directory for SNPs to go in
+# Samples to run
+for i in 1..20;
+
+do
+
+#Sample 1-1 was run as the test
+java -jar GenomeAnalysisTK.jar -T HaplotypeCaller -R $REFDIR/ref.fa -I $INDIR/${i}_recal.bam -dontUseSoftClippedBases \
+-stand_call_conf 20.0 -o $SNPDIR/${i}_SNPs.vcf
+
+#Echo the sample that completes 
+echo ${i}_SNPs.vcf
+
+gzip $SNPDIR/${i}_SNPs.vcf
+
+done
+
+###########################################################################################################################
+# 1e. Variant Filtering
+###########################################################################################################################
+
+# $REFDIR = reference annotation directory
+module load java
+
+#Create a directory or designate a directory for SNPs to go in
+# Samples to run
+for i in 1..20;
+
+do
+
+# Unzip prior to filtering
+gunzip $SNPDIR/${i}_SNPs.vcf
+
+# Filtering commands
+java -jar GenomeAnalysisTK.jar -T VariantFiltration -R $REFDIR/ref.fa -V $SNPDIR/${i}_SNPs.vcf -window 35 -cluster 3 \
+-filterName FS -filter "FS > 30.0" -filterName QD -filter "QD < 2.0" -o $SNPDIR/${i}_SNPs_filtered.vcf
+
+# Echo the sample that completes 
+echo ${i}_SNPs_filtered.vcf
+
+# Rezip files
+gzip $INDIR/*.vcf.gz
+
+done
