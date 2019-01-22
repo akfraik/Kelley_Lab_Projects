624af216ac5a7f697ae17b75b3e0b29c5bdd539d
Alexandra Fraik <alexandra.fraik@wsu.edu>
2019-01-22 14:16:15 -0800

    Update 4a_XTX.sh

--- Lanscape_Genomics/4a_XTX.sh
+++ Lanscape_Genomics/4a_XTX.sh
@@ -44,20 +44,21 @@ with sys.stdout as out:
 """
 
 mkdir -p $SCRIPTDIR
-
 if [[ $PBS_O_WORKDIR ]]; then
 
 ## Load plink
   source /usr/share/modules/init/bash
   module load plink/1.90_extrachr  
+  set -u
+  set -e
   set -o pipefail
   
 ## Loop over populations to get allele count data
    for pop in $POPULATIONS; 
    do
-     keep_file="${KEEPDIR}/${pop}.${i}"
-     count_file="${KEEPDIR}/${pop}.{i}"
-     allele_file="${KEEPDIR}/${pop}.allele.${i}"
+     keep_file="${KEEPDIR}/${pop}.${i}.keep"
+     count_file="${KEEPDIR}/${pop}.${i}.frq.count"
+     allele_file="${KEEPDIR}/${pop}.${i}.allele"
      popfile="${KEEPDIR}/${i}.popfile.tmp"
       p=$(echo $pop)
 
@@ -72,30 +73,31 @@ if [[ $PBS_O_WORKDIR ]]; then
       || { echo "Getting counts for ${pop} failed"; exit 1; }
 
   ## Extract the snps so we know the order of SNPs later
-    awk '{print $2};' "${count_file}.frq.count" > "${OUTDIR}/snps.txt" \
+    awk '{print $2};' "$count_file" > "${OUTDIR}/snps.txt" \
      || { echo "Extracting snps for ${pop} failed"; exit 1; }
 
     ## Now, write a 3rd for loop to produce files format for allele counts for SNPs for population 1
-     file_line_number=$(cat "${OUTDIR}/Freycinet.frq.count" | wc -l)
+     file_line_number=$(cat "${OUTDIR}/$count_file" | wc -l)
         for ((k=2; k<=file_line_number; k++));
          do
-            sed -n ''$k'p' "${OUTDIR}/${pop}.frq.count" | awk -F " " '{print $5}' >> "${OUTDIR}/${pop}.allele.before"
-            sed -n ''$k'p' "${OUTDIR}/${pop}.frq.count" | awk -F " " '{print $6}' >> "${OUTDIR}/${pop}.allele.before"
+            sed -n ''$k'p' "${OUTDIR}/$count_file" | awk -F " " '{print $5}' >> "${OUTDIR}/$allele_file"
+            sed -n ''$k'p' "${OUTDIR}/$count_file" | awk -F " " '{print $6}' >> "${OUTDIR}/$allele_file"
           ## Now we tell the inner loop to run this many times before the outer loop
-            echo "Pass $inner_inner in inner_inner loop."
-            let "inner_inner+=1"  # Increment inner, inner loop counter, or the 3rd level of loops
-          # End of 3rd level loop
+           # End of 3rd level loop
          done
        
     ## Now we tell the inner loop to run this many times before the outer loop
-      echo "Pass $inner in inner loop."
-      let "inner+=1"  # Increment inner loop counter.
      # End of 2nd level loop.
     done 
     
+## Dump the count columns into a temporary file, with tabs separating the entries. The temporary file will need to be
+ # transposed before using it with bayenv.
+  paste -d"\t" "${OUTDIR}/1.${i}.allele" "${OUTDIR}/2.${i}.allele" "${OUTDIR}/3.${i}.allele" "${OUTDIR}/4.${i}.allele" 
+   "${OUTDIR}/5.${i}.allele" "${OUTDIR}/6.${i}.allele" >> "${OUTDIR}/SNPs_file_${i}" \
+  || { echo "extracting output for population failed"; exit 1; }
+    
 ## This "done" complete the for loop for each disease status (pre and post, the initial for loop)        
- # End of outer loop.
- let "outer+=1"    # Increment outer loop counter. 
+ # End of final, outer loop
  done 
  
 else
